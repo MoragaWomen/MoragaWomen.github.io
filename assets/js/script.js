802a2184b0d3c0dc7cfd1eca6111c06a60452936
ryanriddle@berkeley.edu <ryanriddle@berkeley.edu>
2015-08-13 17:48:20 -0700

    changed words and main image, fixed some formatting bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
-// alert("This is nowhere near done. I haven't even put in all the content.");
-// alert("Will probably change everything except navbar.");
-
 var slideimages = new Array() // create new array to preload images
 slideimages[0] = new Image() // create new instance of image object
 slideimages[0].src = "assets/images/main-img1.jpg" // set image object src property to an image's src, preloading that image in the process
 slideimages[1] = new Image()
 slideimages[1].src = "assets/images/main-img2.jpg"
 slideimages[2] = new Image()
-slideimages[2].src = "assets/images/image3.png"
+slideimages[2].src = "assets/images/main-img3.jpeg"
+slideimages[3] = new Image()
+slideimages[3].src = "assets/images/main-img4.jpg"
 var descript = new Array()
 descript[0] = "Image 1"
 descript[1] = "Image 2"
 descript[2] = "Image 3"
+descript[3] = "Image 4"
+
 
 var step = 0
 var whichimage = 0
@@ -23,21 +24,12 @@ function slideit(){
  document.getElementById('slide').src = slideimages[step].src
  document.getElementById('description').innerHTML = descript[step];
  whichimage = step
- if (step<2)
+ if (step<3)
   step++
  else
   step=0
  //call function "slideit()" every 2.5 seconds
  setTimeout("slideit()",2500)
-}
-
-function slidelink(){
- if (whichimage == 0)
-  window.location = "link1.htm"
- else if (whichimage == 1)
-  window.location = "link2.htm"
- else if (whichimage == 2)
-  window.location = "link3.htm"
 }
 
 slideit()
